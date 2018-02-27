bb0408e98667c36b89a95a4c065e62e2ed208040
Evgenii Nasyrov <inasyrov@yandex.ru>
2018-02-27 11:51:05 +0000

    Update the reboot component story

--- components/deckr-reboot/stories.js
+++ components/deckr-reboot/stories.js
@@ -5,11 +5,11 @@ storiesOf('Typography', module)
     .add('Headings', () => (
         <div>
             <h1>h1 heading</h1>
-            <h1>h2 heading</h1>
-            <h1>h3 heading</h1>
-            <h1>h4 heading</h1>
-            <h1>h5 heading</h1>
-            <h1>h6 heading</h1>
+            <h2>h2 heading</h2>
+            <h3>h3 heading</h3>
+            <h4>h4 heading</h4>
+            <h5>h5 heading</h5>
+            <h6>h6 heading</h6>
         </div>
     ))
     .add('Lists', () => (
